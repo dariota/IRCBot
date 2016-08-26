77997d0be4c968b5e2a02d82a600cc91fcdb1fb8
Dário Tavares Antunes <dario@ntun.es>
2016-08-26 12:49:04 +0100

    Added channel persistence.

--- lib/wikiBot.rb
+++ lib/wikiBot.rb
@@ -1,41 +1,43 @@
 require "socket"
+require "set"
 require "~/ruby/shout-bot/lib/mediaFetch"
 
 class WikiBot
-  attr_accessor :channel
+	attr_accessor :channel
 
-  def initialize(server, port, nick, password = nil)
-    @socket = TCPSocket.open(server, port || 6667)
-    @socket.puts "PASSWORD #{password}" if password
-    @socket.puts "NICK #{nick}"
-    @socket.puts "USER #{nick} #{nick} #{nick} :#{nick}"
-    sleep 1
-    self
-  end
+	def initialize(server, port, nick, password = nil)
+		@socket = TCPSocket.open(server, port || 6667)
+		@socket.puts "PASSWORD #{password}" if password
+		@socket.puts "NICK #{nick}"
+		@socket.puts "USER #{nick} #{nick} #{nick} :#{nick}"
+		@channels = Set.new
+		sleep 1
+		self
+	end
 
-  def quit(reason = nil)
-    @socket.puts "QUIT :#{reason or ''}"
-    @socket.gets until @socket.eof?
-  end
+	def quit(reason = nil)
+		@socket.puts "QUIT :#{reason or ''}"
+		@socket.gets until @socket.eof?
+	end
 
-  def join(channel, password = nil)
-    raise ArgumentError unless block_given?
-    params = channel.split('?')
-    @channel = "##{params[0]}"
-    @channel_password = params[1] || password || ""
-    @socket.puts "JOIN #{@channel} #{@channel_password}"
-    yield self
-  end
+	def join(chanName, password = nil)
+		channel = "##{chanName}"
+		@channel_password = password || ""
+		@socket.puts "JOIN #{channel} #{@channel_password}"
+		@channels.add chanName
+	end
 
-  def say(message)
-    return unless @channel
-    @socket.puts "PRIVMSG #{@channel} :#{message}"
-  end
+	def say(message, channel = nil)
+		if channel.nil?
+			raise ArgumentError unless @channels.length == 1
+			channel = @channels.first
+		end
+		join channel unless @channels.member? channel
+		@socket.puts "PRIVMSG #{@channel} :#{message}"
+	end
 
 end
 
 bot = WikiBot.new("irc.netsoc.tcd.ie", 6667, "dariobot")
-bot.join("dariotest") do |bot|
-	bot.say "hello"
-end
+bot.say "Hey bud", "#dariotest"
 bot.quit
