fb3b801aedbf2072c4212e5374d9a8df740ad3e8
Dário Tavares Antunes <dario@ntun.es>
2016-08-28 00:07:36 +0100

    Added message abstraction (doesn't track channel since that's intended to be tracked wherever it's held.

---
+++ lib/Message.rb
@@ -0,0 +1,17 @@
+class Message
+	attr_reader :nick
+	attr_reader :message
+
+	def initialize(nick, message)
+		@nick = nick
+		@message = message
+	end
+
+	def nick_matches(nick)
+		@nick.casecmp(nick) == 0
+	end
+
+	def message_contains(message)
+		!!(@message =~ Regexp.new(Regexp.escape(message)))
+	end
+end
