fccb3abf91e4d7fabd8641efde5f9fef77ab462e
Dário Tavares Antunes <dario@ntun.es>
2016-08-28 00:41:18 +0100

    Added Logging class to handle management of logged messages.

---
+++ lib/Logger.rb
@@ -0,0 +1,34 @@
+require "./RingBuffer"
+require "./Message"
+
+LOG_LENGTH = 100
+
+class Logger
+
+	def initialize(channels = nil)
+		@logs = Hash.new
+		channels.each {|c| add_channel c} unless channels.nil?  
+	end
+
+	def add_channel(channel)
+		@logs[channel.downcase] = RingBuffer.new LOG_LENGTH unless channel.nil?
+	end
+
+	def log(channel, nick, message)
+		if @logs[channel.downcase].nil?
+			add_channel channel
+		end
+
+		logs = @logs[channel.downcase]
+		logs << Message.new(nick, message)
+	end
+
+	def find(channel, nick, messagePart)
+		if @logs[channel.downcase].nil? # should be impossible, but hey
+			return nil
+		end
+
+		@logs[channel.downcase].find { |m| m.nick_matches(nick) && m.message_contains(messagePart) }
+	end
+
+end
